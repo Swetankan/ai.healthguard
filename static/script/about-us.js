795e5a78950c9afb24922b1797c8dcf2ab2aa08b
rohit-447 <rohitkumar826344@gmail.com>
2024-06-26 15:11:34 +0530

    test

---
+++ static/script/about-us.js
@@ -0,0 +1,13 @@
+var contactText = document.getElementById("contactText");
+      if (contactText) {
+        contactText.addEventListener("click", function (e) {
+          window.location.href = "./contact-us.html";
+        });
+      }
+      
+      var dashboardText = document.getElementById("dashboardText");
+      if (dashboardText) {
+        dashboardText.addEventListener("click", function (e) {
+          // Please sync "Dashboard" to the project
+        });
+      }
---
+++ static/script/contact-us.js
@@ -0,0 +1,36 @@
+var healthGuardAiBgWhiteRemovImage = document.getElementById(
+    "healthGuardAiBgWhiteRemovImage"
+  );
+  if (healthGuardAiBgWhiteRemovImage) {
+    healthGuardAiBgWhiteRemovImage.addEventListener("click", function (e) {
+      window.location.href = "./home-page.html";
+    });
+  }
+  
+  var healthGuardText = document.getElementById("healthGuardText");
+  if (healthGuardText) {
+    healthGuardText.addEventListener("click", function (e) {
+      window.location.href = "./home-page.html";
+    });
+  }
+  
+  var homeText = document.getElementById("homeText");
+  if (homeText) {
+    homeText.addEventListener("click", function (e) {
+      window.location.href = "./home-page.html";
+    });
+  }
+  
+  var aboutText = document.getElementById("aboutText");
+  if (aboutText) {
+    aboutText.addEventListener("click", function (e) {
+      window.location.href = "./about-us.html";
+    });
+  }
+  
+  var dashboardText = document.getElementById("dashboardText");
+  if (dashboardText) {
+    dashboardText.addEventListener("click", function (e) {
+      // Please sync "Dashboard" to the project
+    });
+  }
